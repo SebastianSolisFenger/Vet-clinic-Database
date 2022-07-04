b360fb9864f584bdbff4381b2dff91ff95ef4621
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-04 19:23:23 -0300

    CODE QUERIES

--- queries.sql
+++ queries.sql
@@ -59,3 +59,33 @@ COMMIT TRANSACTION;
 
 SELECT * FROM animals;
 
+-- 'QUERIES'
+
+--// How many animals are there?
+
+SELECT COUNT(*) FROM animals;
+
+--// How many animals have never tried to escape?
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+--// What is the average weight of animals?
+
+SELECT AVG(weight_kg) FROM animals;
+
+--// Who escapes the most, neutered or not neutered animals?
+
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+
+--// What is the minimum and maximum weight of each type of animal?
+
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+
+--// What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+
+--// What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth <= '2000-12-31' AND date_of_birth >= '1990-01-01' GROUP BY species;
+
