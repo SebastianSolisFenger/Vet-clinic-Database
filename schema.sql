f667079e08a0cd1367892e52d2111d7d96b37787
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-04 10:37:11 -0300

    Create a database named vet_clinic and create a table named animals.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,16 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals (
     name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
+    PRIMARY KEY (id)
 );
+
+
