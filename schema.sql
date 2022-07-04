1f872dc950eb94eebea11c0079a2e27185220509
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-04 13:15:48 -0300

    fix name

--- schema.sql
+++ schema.sql
@@ -3,7 +3,6 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL,
