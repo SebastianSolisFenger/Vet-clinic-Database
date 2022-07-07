12db230ffdcaf3f76990b6dd39dde50df11e4e4e
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-07 11:42:09 -0300

    Add table visits that forgot to before

--- schema.sql
+++ schema.sql
@@ -33,3 +33,4 @@ CREATE TABLE vets (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), age I
 
 CREATE TABLE specializations (id INT GENERATED ALWAYS AS IDENTITY, species_id INT, vet_id INT, FOREIGN KEY (species_id) REFERENCES species (id), FOREIGN KEY (vet_id) REFERENCES vets (id)); 
 
+CREATE TABLE visits (id INT GENERATED ALWAYS AS IDENTITY, date_of_visit DATE, animal_id INT, vet_id INT, PRIMARY KEY (id), FOREIGN KEY (animal_id) REFERENCES animals (id), FOREIGN KEY (vet_id) REFERENCES vets (id)); 
