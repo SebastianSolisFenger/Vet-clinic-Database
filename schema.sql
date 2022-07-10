86bcab64c8cf22b22e94d4fc4e565697505f1656
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-10 14:23:30 -0300

    Add an email column to your owners table

--- schema.sql
+++ schema.sql
@@ -36,3 +36,7 @@ CREATE TABLE specializations (id INT GENERATED ALWAYS AS IDENTITY, species_id IN
 
 CREATE TABLE visits (id INT GENERATED ALWAYS AS IDENTITY, date_of_visit DATE, animal_id INT, vet_id INT, 
 PRIMARY KEY (id), FOREIGN KEY (animal_id) REFERENCES animals (id), FOREIGN KEY (vet_id) REFERENCES vets (id)); 
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
